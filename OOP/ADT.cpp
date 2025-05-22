f43873dcb30e743bef3cc5e8212e2febd78b7cf4
MrHDMai <m.haodmai@gmail.com>
2025-05-21 23:28:34 -0700

    Inheritence

--- OOP/ADT.cpp
+++ OOP/ADT.cpp
@@ -1,23 +1,49 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-class animal{
+class family {
 private:
-    string breed;
-
+    string myfamily;
 public:
-    void setname(string mybreed){
-        breed = mybreed;
+    void setfamily(string member) {
+        myfamily = member;
     }
-    string getname(){
-        return breed;
+    string getfamily() {
+        return myfamily;
     }
 };
 
-int main(){
-    animal dog = animal();
-    dog.setname("Pitbull");
-    cout << "The dog's breed is: \n" <<dog.getname() << endl;
+class pet {
+private:
+    string petname;
+public:
+    pet(string name) : petname(name) {}
+    virtual string anmlsound() = 0; // Pure virtual function
+    void sname(string name) {
+        petname = name;
+    }
+    string gname() {
+        return petname;
+    }
+};
+
+class dog : public pet { // Renamed to 'dog' for clarity
+public:
+    dog(string name) : pet(name) {}
+    string anmlsound() override { // Correctly overriding the pure virtual function
+        return "woof woof";
+    }
+};
+
+int main() {
+    family fm;
+    fm.setfamily("Dad");
+    cout << "The first person in the family is: " << fm.getfamily() << endl;
+
+    dog myDog("Hound"); // Using 'dog' instead of 'anmlsound'
+    cout << myDog.gname() << " says: " << myDog.anmlsound() << endl;
+
+    return 0;
 }
