f0ce427b0464ccad3357b4d227ffe8b2cfced985
MrHDMai <m.haodmai@gmail.com>
2025-05-23 23:25:25 -0700

    More on ADT

--- OOP/ADT.cpp
+++ OOP/ADT.cpp
@@ -10,13 +10,22 @@ public:
     void setfamily(string member) {
         myfamily = member;
     }
+    virtual string nmember() = 0;
     string getfamily() {
         return myfamily;
     }
 };
 
+class fmember : public family{
+public:
+    fmember(string member){setfamily(member);}// since family doesn't have a constructor that would return a string we have to inti set family here
+    string nmember() override{
+        return "mom";
+    }
+};
+
 class pet {
-private:
+protected:
     string petname;
 public:
     pet(string name) : petname(name) {}
@@ -32,18 +41,49 @@ public:
 class dog : public pet { // Renamed to 'dog' for clarity
 public:
     dog(string name) : pet(name) {}
-    string anmlsound() override { // Correctly overriding the pure virtual function
+    void operator = (const dog &d){ //operator overload
+        petname = d.petname;
+    }
+    string anmlsound() override {  //overriding the pure virtual function
         return "woof woof";
     }
 };
 
+class cat : public pet{
+public:
+    cat(string name) : pet(name){}
+    void operator = (const cat &c){
+        petname = c.petname;
+    }
+    string anmlsound() override{
+        return "Meow Meow";
+    }
+};
+
+template<typename T>
+void getname_sound(T &thepet){
+    cout << thepet.anmlsound() << endl;
+    cout << thepet.gname() << endl;
+}
+
 int main() {
-    family fm;
-    fm.setfamily("Dad");
+    fmember fm("Dad");
     cout << "The first person in the family is: " << fm.getfamily() << endl;
+    fmember fm2("Mom");
+    cout << "The second person in my family is: " << fm2.getfamily() << endl;
 
     dog myDog("Hound"); // Using 'dog' instead of 'anmlsound'
     cout << myDog.gname() << " says: " << myDog.anmlsound() << endl;
 
+    dog myd = dog("Terrier");
+    cout << myd.gname() << " is barking: ";
+    cout << myd.anmlsound() << endl;
+
+    dog myd2 = dog("Pit");
+    cout << myd2.gname() << " is barking: ";
+    cout << myd2.anmlsound() <<endl;
+
+    cat c = cat("Prince");
+    getname_sound(c);
     return 0;
 }
