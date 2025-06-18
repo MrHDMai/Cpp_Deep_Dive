f4f371ee19c6682cb78d38ff06e7d0945e6d93df
MrHDMai <m.haodmai@gmail.com>
2025-06-17 22:46:53 -0700

    Templating

--- Hackerrank/FindMaxofContinguousWindow.cpp
+++ Hackerrank/FindMaxofContinguousWindow.cpp
@@ -1,38 +1,35 @@
-#include<iostream>
-#include<deque>
+    #include<iostream>
+    #include<deque>
 
-using namespace std;
+    using namespace std;
 
-void printmax(int arr[], int n, int k){
-    deque<int> dq;
-    for(int i = 0; i < n; i++){
-        if(!dq.empty() && dq.front() <= i - k){
-        dq.pop_front();
-    }
-        while(!dq.empty() && arr[i] >= arr[dq.back()])
-            dq.pop_back();
-    dq.push_back(i);
-    if(i >= k - 1){
-        cout << arr[dq.front()] << " ";
-        }
-    }
-    cout << endl;
-}
-
-int main(){
-    int t;
-    cin >>t;
-    while(t > 0){
-        int n, k;
-        cin >> n >> k;
-        int i;
-        int arr[i];
+    void printmax(int arr[], int n, int k){
+        deque<int> dq;
         for(int i = 0; i < n; i++){
-            cin >> arr[i];
+            while(!dq.empty() && dq.front() <= i - k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && arr[i] >= arr[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i >= k - 1){
+                cout<< arr[dq.front()] << " ";
+            }
+            cout << endl;
         }
-        printmax(arr,n,k);
-        t--;
     }
-    return 0;
-}
 
+    int main(){
+        int t;
+        cin >> t;
+        while(t--){
+            int n, k;
+            cin >> n >> k;
+            int arr[n];
+            for(int i = 0; i < n; i++){
+                cin >> arr[i];
+            }
+            printmax(arr,n,k);
+        }
+    }
