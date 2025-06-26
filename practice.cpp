10f18bbbc7ebf8c7a83c0d789b90483c325270bd
MrHDMai <m.haodmai@gmail.com>
2025-06-25 22:49:57 -0700

    Prime Composition

--- practice.cpp
+++ practice.cpp
@@ -1,64 +1,55 @@
 #include<iostream>
 #include<vector>
-#include<queue>
-#include<limits>
 
 using namespace std;
 
-typedef pair<int,int> node_dist;
-
-class graph{
-    int vertices;
-    vector<vector<node_dist>> adj;
+class matrix{
 public:
-    graph(int vertices) : vertices(vertices), adj(vertices){};
-    void addedges(int u, int v, int w);
-    void dijkstra(int src);
+    vector<vector<int>> a;
+    matrix operator*(const matrix& other){
+        matrix result;
+        int n = a.size();
+        int m = a[0].size();
+        for(int i = 0; i < n; i++){
+            vector<int>row(m);
+            for(int j = 0;  j < m; j++){
+                row[j] = a[i][j] * other.a[i][j];
+            }
+            result.a.push_back(row);
+        }
+        return result;
+    }
 };
 
-void graph :: addedges(int u, int v, int w){
-    adj[u].emplace_back(v,w);
-    adj[v].emplace_back(u,w);
-};
-
-void graph :: dijkstra(int src){ 
-    priority_queue <node_dist, vector<node_dist>, greater<node_dist>> pq;
-    vector<int> dist(vertices,numeric_limits<int>::max());
-    pq.push({0,src});
-    dist[src] = 0;
-    while(!pq.empty()){
-        int u = pq.top().second;
-        pq.pop();
-        for(auto& neighbor : adj[u]){
-            int v = neighbor.first;
-            int weight = neighbor.second;
-            if(dist[v] > dist[u] + weight){
-                dist[v] = dist[u] + weight;
-                pq.push({dist[v],v});
+int main(){
+    int cases,k;
+    cin >> cases;
+    for(k = 0; k < cases; k++){
+        matrix result;
+        matrix x;
+        matrix y;
+        int m, n;
+        cin >> m >> n;
+        for(int i = 0; i < n; i++){
+            vector<int> b(m);
+            for(int j = 0; j < m; j++){
+                cin >> b[j];
+            }
+            x.a.push_back(b);
+        }
+        for(int i = 0; i < n; i++){
+            vector<int>b(m);
+            for(int j = 0; j < m; j++){
+                cin >> b[j];
+            }
+            y.a.push_back(b);
+        }
+        result = x * y;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                cout << result.a[i][j] << endl;
             }
         }
     }
-    for(int i = 0; i < vertices; i++){
-        if(i < numeric_limits<int>::max()){
-            cout << "The distance from " << i << " is " << dist[i] << endl;
-        } else {
-            cout << "The distance to " << i << " is impossible";
-        }
-    }
-}
-
-
-int main(){
-    int n, m, u,w,v;
-    cin >> n,m;
-    graph g(n);
-    cout << "enter the edges of the graph (u, v, w)";
-    cin >> u >> v >> w;
-    for(int i = 0; i < m; i++){
-        g.addegdes(u,v,w);
-    }
-    int src;
-    cout << "Enter the src" << endl;
-    cin >> src;
-    g.dijkstra(int src);
+    return 0;
 }
