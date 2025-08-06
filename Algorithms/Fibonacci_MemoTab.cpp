5b431d336e7cfcaae2376522241b698b5f586c2d
MrHDMai <m.haodmai@gmail.com>
2025-08-05 22:52:58 -0700

    Fib spsace optimization

--- Algorithms/Fibonacci_MemoTab.cpp
+++ Algorithms/Fibonacci_MemoTab.cpp
@@ -25,6 +25,18 @@ int fibtb(int n){
     
 }
 
+//space optimization
+int fibsp(int n){
+    if(n <= 1) return n;
+    int prev2 = 0, prev1 = 1, curr;
+    for(int i = 2, i <= n; i++){
+        curr = prev + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
+
 int main(){
     int n;
     cout << "Enter n: ";
