6bf312e736dbf9ae5b530b789f4851be7e89dd1c
MrHDMai <m.haodmai@gmail.com>
2025-08-26 22:13:24 -0700

    Heap Sort Fix

--- Algorithms/heapsort.cpp
+++ Algorithms/heapsort.cpp
@@ -12,20 +12,20 @@ void heapify(int arr[], int n, int i){
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
-    if(left < n && arr[left] > arr[largest])largest = left;
-    if(right < n && arr[right] > arr[largest]) largest = right;
+    if(left < n && arr[left] > arr[large])large = left;
+    if(right < n && arr[right] > arr[large]) large = right;
 
-    if(largest!= i){
-        swap(&arr[i],&arr[largest]);
-        heapify(arr,n,largest);
+    if(large!= i){
+        swap(&arr[i],&arr[large]);
+        heapify(arr,n,large);
     }
 }
 
 void heapsort(int arr[], int n){
-    for(int i = n/2 - 1; i > 0; i--)
+    for(int i = n/2 - 1; i >=0; i--)
         heapify(arr,n,i);
     
-    for(int i = n-1; i > 0; i--){
+    for(int i = n-1; i >= 0; i--){
         swap(&arr[0], &arr[i]);
         heapify(arr,i,0);
     }
@@ -35,7 +35,7 @@ int main() {
     int arr[] = {4, 10, 3, 5, 1};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    heapSort(arr, n);
+    heapsort(arr, n);
 
     for (int i = 0; i < n; ++i) cout << arr[i] << " ";
     return 0;
