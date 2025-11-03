e357e96bdd107e1f95d793526c58d61eee2970e0
MrHDMai <m.haodmai@gmail.com>
2025-11-02 22:32:10 -0800

    Binary search tree fix

--- Algorithms/binarysearch.cpp
+++ Algorithms/binarysearch.cpp
@@ -12,7 +12,7 @@ node* insert(node* root, int key){
     if(root == nullptr) return new node(key);
     if(key < root->key)
         root->left = insert(root->left, key);
-    else if(key -> root->key)
+    else if(key > root->key)
         root->right = insert(root->right, key);
 
     return root;
