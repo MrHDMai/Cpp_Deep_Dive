dbce31ae78b2c000627099ed95b1ae79d6a783b4
MrHDMai <m.haodmai@gmail.com>
2026-05-28 01:23:16 -0700

    sol

--- LeetCode/AddingNum.cpp
+++ LeetCode/AddingNum.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 class addingNum{
+public: 
     vector<int> addnum(vector<int>& num, int target){
         for(int i = 0; i < num.size(); i++){
             for(int j = i + 1; j < num.size(); j++){
@@ -14,3 +15,17 @@ class addingNum{
         return {};
     }
 };
+
+int main(){
+    int target = 11;
+    vector<int> v{2,3,5,6,7,8,1,3,4,6};
+    addingNum ad;
+    vector<int> result = ad.addnum(v,target);
+    int index1 = result[0];
+    int index2 = result[1];
+    int* val1 = &v[index1];
+    int* val2 = &v[index2];
+
+    cout << result[0]<<":"<< *val1<<" "<<result[1]<<":" <<*val2;
+
+} 
